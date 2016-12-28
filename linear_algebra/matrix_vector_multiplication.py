73ccef3d4fcfbce4d2726a27e1ac70c8075bc593
Anthony Lee <antwonlee@gmail.com>
2016-12-28 02:20:26 -0600

    Add matrix-vector-multiplication

---
+++ linear_algebra/matrix_vector_multiplication.py
@@ -0,0 +1,23 @@
+matrix = [[1,2,1,5], [0,3,0,4], [-1, -2, 0, 0]]
+vector = [1,3,2,1]
+
+#multiply matrix x vector
+
+#loop through first line 
+#(1x1) + (2x3) + (1x2) + (5x1)
+#sum
+#1 + 6 + 2 + 5 = 14
+
+#loop through second line
+#(0x1) + (3x3) + (0x2) + (4x1)
+#sum
+#0 + 9 + 0 + 4 = 13
+
+#loop through third line
+#(-1x1) + (-2x3) + (0x2) + (0x1)
+#sum
+#-1 + -6 + 0 + 0 = -7
+
+#new result
+# [14, 13 ,-7]
+
