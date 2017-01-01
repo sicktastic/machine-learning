162dfefc35a045558ef79bc1cd1c8b23a9feee89
Anthony Lee <antwonlee@gmail.com>
2016-12-31 23:02:51 -0600

    Add median

--- playground/mean_median_mode.py
+++ playground/mean_median_mode.py
@@ -1,14 +1,21 @@
-numbers = [i**2 for i in range(10)]
+range_iteration = 210
+numbers = [i**2 for i in range(range_iteration)]
 
 def mean():
     return sum(numbers) / len(numbers)
 
-print(mean())
-
 def median():
     numbers.sort()
 
-print(median())
+    if (len(numbers) % 2 == 0):
+        first_middle_number = (numbers[len(numbers)/2])
+        second_middle_number = (numbers[(len(numbers)/2)+1])
+        median = (first_middle_number + second_middle_number) / 2.0
+        return median
+    else:
+        median = numbers[len(numbers) / 2]
+        return median
 
-def mode():
-    numbers.sort()
+print(numbers)
+print(mean())
+print(median())
