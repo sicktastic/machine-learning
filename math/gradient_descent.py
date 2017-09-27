24f893b01c36715fa3df9355b26afea76041d2bb
Anthony Lee <antwonlee@gmail.com>
2017-09-27 00:55:43 -0500

    Fix python syntax error

--- math/gradient_descent.py
+++ math/gradient_descent.py
@@ -25,7 +25,7 @@ def step_gradient(b_current, m_current, points, learningRate):
 def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
     b = starting_b
     m = starting_m
-    for i in range(num_iterations)
+    for i in range(num_iterations):
         b, m = step_gradient(b, m, array(points), learning_rate)
     return [b, m]
 
