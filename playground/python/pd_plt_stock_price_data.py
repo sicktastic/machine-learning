a0bc028711397834ac727b77aaadbf16bff5635f
Anthony Lee <antwonlee@gmail.com>
2018-01-09 01:50:11 -0600

    Add basic pd and plt for stock price

---
+++ playground/python/pd_plt_stock_price_data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def test_run():
+    df = pd.read_csv("../../datasets/HCP.csv")
+    print(df['Adj Close'])
+    df['Adj Close'].plot()
+    plt.show()
+
+if __name__ == "__main__":
+    test_run()
