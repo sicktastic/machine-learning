6f1086edf17a68e47bfca90f556fdf48920386a3
Anthony Lee <antwonlee@gmail.com>
2018-01-09 01:52:21 -0600

    Add stock datasets

--- playground/python/pd_plt_stock_price_data.py
+++ playground/python/pd_plt_stock_price_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def test_run():
-    df = pd.read_csv("../../datasets/HCP.csv")
+    df = pd.read_csv("../../datasets/stock/AAPL.csv")
     print(df['Adj Close'])
     df['Adj Close'].plot()
     plt.show()
