34e9c806e915db6d1e80949293f116ad43dfcfbd
Anthony Lee <antwonlee@gmail.com>
2018-01-09 22:53:24 -0600

    Add utility function quiz

--- playground/python/pd_utility_functions.py
+++ playground/python/pd_utility_functions.py
@@ -14,5 +14,31 @@ def get_data(symbols, dates):
         symbols.insert(0, 'SPY')
 
     for symbol in symbols:
+        df_temp = pd.read_csv(symbol_to_path(symbol),
+                         index_col = 'Date', 
+                         parse_dates = True, 
+                         usecols = ['Date', 'Adj Close'], 
+                         na_values = ['nan'])
+        df_temp = df_temp.rename(columns = {'Adj Close': symbol})
+        # df = df.join(df_temp, how = "inner")
+        df = df.join(df_temp)
+        if symbol == 'SPY':
+            df = df.dropna(subset = ['SPY'])
 
     return df
+
+def test_run():
+    # Define a date range
+    dates = pd.date_range('2010-01-22', '2010-01-26')
+
+    # Choose stock symbols to read
+    symbols = ['GOOG', 'IBM', 'GLD']
+
+    # Get stock data
+    df = get_data(symbols, dates)
+
+    print(df)
+
+
+if __name__ == "__main__":
+    test_run()
