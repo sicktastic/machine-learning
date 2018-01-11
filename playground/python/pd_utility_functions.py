0677bb489b772fe5c14a48c1fec94654205a5062
Anthony Lee <antwonlee@gmail.com>
2018-01-11 00:56:03 -0600

    Add pandas slice and plot

--- playground/python/pd_utility_functions.py
+++ playground/python/pd_utility_functions.py
@@ -2,6 +2,7 @@
 
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def symbol_to_path(symbol, base_dir="../../datasets/stock/"):
     """ Returns CSV file path given ticker symbol """
@@ -26,6 +27,13 @@ def get_data(symbols, dates):
             df = df.dropna(subset = ['SPY'])
 
     return df
+
+def plot_data(df):
+    ''' Plot stock prices '''
+    ax = df.plot(title = title)
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Price")
+    plt.show()
 
 def test_run():
     # Define a date range
