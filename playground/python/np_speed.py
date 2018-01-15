e77af87d7b2644f2189f30718bf83b875feb9c8c
Anthony Lee <antwonlee@gmail.com>
2018-01-14 22:34:26 -0600

    Add np argmax and time for np

---
+++ playground/python/np_speed.py
@@ -0,0 +1,10 @@
+import time
+
+def test_run():
+    t1 = time.time()
+    print("ML4T")
+    t2 = time.time()
+    print("The time taken by print statement is ", t2 - t1," seconds")
+
+if __name__ == '__main__':
+  test_run()
