d7658eda5c3e248fd8374abaa363020b9e269b78
Anthony Lee <antwonlee@gmail.com>
2018-01-16 02:23:40 -0600

    Add bollinger bands

--- playground/python/pd_calculate_bollinger_bands.py
+++ playground/python/pd_calculate_bollinger_bands.py
@@ -42,11 +42,14 @@ def get_rolling_mean(values, window):
 def get_rolling_std(values, window):
     """Return rolling standard deviation of given values, using specified window size."""
     # TODO: Compute and return rolling standard deviation
+    return pd.rolling_std(values, window=window)
 
 
 def get_bollinger_bands(rm, rstd):
     """Return upper and lower Bollinger Bands."""
     # TODO: Compute upper_band and lower_band
+    upper_band = rm + rstd * 2
+    lower_band = rm - rstd * 2
     return upper_band, lower_band
 
 
