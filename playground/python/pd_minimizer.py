66cf7e0e21dc66b72973cc451af95e65e8a35adb
Anthony Lee <antwonlee@gmail.com>
2018-01-23 23:07:53 -0600

    Add minimizer plot

--- playground/python/pd_minimizer.py
+++ playground/python/pd_minimizer.py
@@ -15,5 +15,13 @@ def test_run():
     print("Minima found at:")
     print("X = {}, Y= {}".format(min_result.x, min_result.fun))
 
+    # Plot function values, mark minima
+    Xplot = np.linspace(0.5, 2.5, 21)
+    Yplot = f(Xplot)
+    plt.plot(Xplot, Yplot)
+    plt.plot(min_result.x, min_result.fun, 'ro')
+    plt.title("Minima of an objective function")
+    plt.show()
+
 if __name__ == '__main__':
     test_run()
